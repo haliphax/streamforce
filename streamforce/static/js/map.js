786db0e01aa2f9f06a3ceec0159db7cbc16f6533
haliphax <haliphax@gmail.com>
2021-09-27 20:49:38 -0500

    💄 1080p canvas, scale sprites in game engine

--- streamforce/static/js/map.js
+++ streamforce/static/js/map.js
@@ -1,13 +1,6 @@
-class Game extends Phaser.Scene
-{
+class Game extends Phaser.Scene {
 	constructor() {
 		super();
-	}
-
-	preload() {
-		this.load.setBaseURL('/assets');
-		this.load.spritesheet(
-			'tiles', 'sprites.png', { frameWidth: 16 });
 	}
 
 	create() {
@@ -18,47 +11,53 @@ class Game extends Phaser.Scene
 				key: 'blue',
 				frames: this.anims.generateFrameNumbers(
 					'tiles', { frames: [354, 357, 358, 357] }),
-				frameRate: 4,
+				frameRate: 6,
 				repeat: -1,
 			}),
 			this.anims.create({
 				key: 'yellow',
 				frames: this.anims.generateFrameNumbers(
 					'tiles', { frames: [402, 405, 406, 405] }),
-				frameRate: 4,
+				frameRate: 6,
 				repeat: -1,
 			}),
 			this.anims.create({
 				key: 'green',
 				frames: this.anims.generateFrameNumbers(
 					'tiles', { frames: [450, 453, 454, 453] }),
-				frameRate: 4,
+				frameRate: 6,
 				repeat: -1,
 			}),
 		];
 
 		let count = 0;
 
-		for (let i = 0; i < 270; i += 16)
-			for (let j = 0; j < 480; j += 16) {
+		for (let i = 0; i < 1080; i += 64)
+			for (let j = 0; j < 1920; j += 64) {
 				const delay = Math.random() * 1000;
 				const choice = sprites[Math.floor(Math.random() * sprites.length)];
 
 				this.add.sprite(j, i, 'tiles', choice.frames[0].textureFrame)
 					.setOrigin(0, 0)
+					.setScale(4)
 					.playAfterDelay(choice, delay);
 				count++;
 			}
 
 		console.log(`${count} sprites added to scene`);
 	}
+
+	preload() {
+		this.load.setBaseURL('/assets');
+		this.load.spritesheet(
+			'tiles', 'sprites.png', { frameWidth: 16 });
+	}
 }
 
-const config = {
+const game = new Phaser.Game({
+	height: 1080,
+	pixelArt: true,
+	scene: [Game],
 	type: Phaser.AUTO,
-	width: 480,
-	height: 270,
-	scene: [ Game ],
-};
-
-const game = new Phaser.Game(config);
+	width: 1920,
+});
